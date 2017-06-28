dd89dd62121295143fb238bba2bb709f70df9cf1
Fraideron <jiafen24@gmail.com>
2017-06-28 17:39:50 +0300

    fixed bugs

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,19 +1,13 @@
-/* global malarkey:false, moment:false */
 
-import { config } from './index.config';
 import { routerConfig } from './index.route';
-import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { ControllPanel } from './cp/cp.controller';
 
 import { UserService } from '../app/components/user.service';
 import { TranslationService } from '../app/components/translation.service';
 
-
-angular.module('eutaTestProject', ['ui.router', 'toastr'])
-  .config(config)
+angular.module('eutaTestProject', ['ui.router'])
   .config(routerConfig)
-  .run(runBlock)
   .service('user', UserService)
   .service('translation', TranslationService)
   .controller('MainController', MainController)
